afc600d1dd93b49e91442b4a5e5ff6e16a031276
Taleh Taghiyev <taleh958@gmail.com>
2022-02-27 20:03:35 +0400

    This is functional Laravel REST API project for resizing the images by link or by uploading

---
+++ app/Http/Resources/V1/AlbumResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AlbumResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'crated_at' => $this->crated_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
